94fbf63a3c5adf0712bf06a0b2bd542a16bb8e40
Zerofran <144761501+Zerofran@users.noreply.github.com>
2025-09-07 13:29:28 -0600

    Update

--- ToolScript/setting.gd
+++ ToolScript/setting.gd
@@ -1,8 +1,18 @@
 extends Control
 
+@onready var foto : TextureRect = $"../ScrollContainer/HBoxContainer/PrincipalContainer_V/Encavezado/Foto_Estudiante"
+
 func _ready() -> void:
 	visible = true
 	
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("tap"):
 		self.visible =! self.visible
+
+
+func _on_foto_pressed() -> void:
+	$"../FileDialog".visible = true
+
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	pass # Replace with function body.
