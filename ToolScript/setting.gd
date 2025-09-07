b28d2a9ecf9ce18e325a15129e4885e4fe137021
Zerofran <144761501+Zerofran@users.noreply.github.com>
2025-09-07 13:51:30 -0600

    Carga de imagen Exitosa

--- ToolScript/setting.gd
+++ ToolScript/setting.gd
@@ -1,18 +1,34 @@
 extends Control
 
-@onready var foto : TextureRect = $"../ScrollContainer/HBoxContainer/PrincipalContainer_V/Encavezado/Foto_Estudiante"
+@onready var foto_Estudiante : TextureRect = $"../ScrollContainer/HBoxContainer/PrincipalContainer_V/Encavezado/Foto_Estudiante"
 
 func _ready() -> void:
 	visible = true
 	
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("tap"):
-		self.visible =! self.visible
+		self.visible = !self.visible
 
 
 func _on_foto_pressed() -> void:
+	# Muestra el FileDialog.
 	$"../FileDialog".visible = true
 
 
 func _on_file_dialog_file_selected(path: String) -> void:
-	pass # Replace with function body.
+	# Crea un nuevo objeto Image para cargar los datos del archivo.
+	var image = Image.new()
+	var error = image.load(path)
+	
+	# Verifica si la carga fue exitosa.
+	if error != OK:
+		print("Error al cargar la imagen: ", error)
+		return
+
+	# Crea una nueva textura a partir de los datos de la imagen.
+	var new_texture = ImageTexture.create_from_image(image)
+	
+	# Verifica que la textura sea válida y asígnala al TextureRect.
+	if new_texture:
+		foto_Estudiante.texture = new_texture
+		print("Imagen cargada con éxito.")
